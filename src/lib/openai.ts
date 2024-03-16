946c764d011a3edfd780302700c24588f04eb54f
Indu-Chandana <indu.99.ic@gmail.com>
2024-03-16 13:58:03 +0530

    Mar16 001 generate image and save created notebook in neonDB

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -28,6 +28,26 @@ export async function generateImagePrompt(name: string) {
         const data = await response.json()
 
         console.log('openAI json res ::', data)
+
+        /////////////////////////////////////////////////////////////////////////////
+        // openAI json res :: {
+        //     id: 'chatcmpl-93J5KrX80OUC9A2o6GXf7xRHa2qgk',
+        //     object: 'chat.completion',
+        //     created: 1710575374,
+        //     model: 'gpt-3.5-turbo-0125',
+        //     choices: [
+        //       {
+        //         index: 0,
+        //         message: [Object],
+        //         logprobs: null,
+        //         finish_reason: 'stop'
+        //       }
+        //     ],
+        //     usage: { prompt_tokens: 65, completion_tokens: 10, total_tokens: 75 },
+        //     system_fingerprint: 'fp_4f2ebda25a'
+        //   }
+        /////////////////////////////////////////////////////////////////////////////
+
         const image_description = data.choices[0].message.content
         return image_description as string
     } catch (error) {
@@ -36,6 +56,30 @@ export async function generateImagePrompt(name: string) {
     }
 }
 
-export async function generateImage() {
+export async function generateImage(image_description: string) {
+    try {
+        const response = await openai.createImage({
+            prompt: image_description,
+            n: 1, // how many images are needed
+            size: '256x256'
+        })
+        const data = await response.json()
 
+        /////////////////////////////////////////////////////////////////////////////
+        // res json from open-ai image ::  {
+        //     created: 1710575382,
+        //     data: [
+        //       {
+        //         url: 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-vcgYHtTCckxYQm47B5TG69Pa/user-eDTTCUMQ0457ZXkr1SR0Uysy/img-0SKN6IgSVuSwS6nS7EyqVe2g.png?st=2024-03-16T06%3A49%3A42Z&se=2024-03-16T08%3A49%3A42Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-03-16T07%3A15%3A09Z&ske=2024-03-17T07%3A15%3A09Z&sks=b&skv=2021-08-06&sig=U1ZfO%2B//kYuvTjUJjxpGQyZR1bGt%2Bkkq8KDysFa2f0M%3D'
+        //       }
+        //     ]
+        //   }
+        /////////////////////////////////////////////////////////////////////////////
+
+        console.log('res json from open-ai image :: ', data)
+        const image_url = data.data[0].url
+        return image_url as string
+    } catch (error) {
+        console.error(error)
+    }
 }
