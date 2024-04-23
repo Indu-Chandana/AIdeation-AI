27a07f0d186ccc48f0da3f2e44adc534b8b2218e
I.Chandana <72302495+Indu-Chandana@users.noreply.github.com>
2024-04-23 17:08:31 +0530

    Update provider.tsx

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,3 +1,28 @@
+// 'use client'
+// import React from 'react'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// type Props = {
+//     children: React.ReactNode
+// }
+
+// const queryClient = new QueryClient();
+
+// // why we use that -@tanstack/react-query- 
+// // u are fetching note from api, after u navigate to another page and u are fetching same endpoint and getting back the same endpoint.
+// // instered of doingthis 
+// // react query will notice that and getting that data from the cache.
+
+// const Provider = ({ children }: Props) => {
+//     return (
+//         <QueryClientProvider client={queryClient}>
+//             {children}
+//         </QueryClientProvider>
+//     )
+// }
+
+// export default Provider
+
 'use client'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
